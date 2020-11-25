52d2f9dad4577dde8776c9a64ebf5554e1564035
Zaynah Javed <zjaved@berkeley.edu>
2020-11-25 14:52:00 -0800

    new bash file to run all commands

---
+++ run.sh
@@ -0,0 +1,6 @@
+roslaunch stdr_launchers among_us.launch &
+source spawn_robots.bash
+python among_us/src/rviz_track_position.py &
+python among_us/src/taskmaster.py &
+roslaunch stdr_launchers among_rviz.launch &
+roslaunch stdr_launchers robot_control.py
