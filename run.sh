fd458d0bd8bbe7c6b2392a8979f2c6a72c36e3d1
Krish Nayar <krish.nayar@berkeley.edu>
2020-12-04 08:22:25 -0800

    added lab8_starter

--- run.sh
+++ run.sh
@@ -2,6 +2,5 @@ roslaunch stdr_launchers among_us.launch &
 source spawn_robots.bash
 python among_us/src/rviz_track_position.py &
 python among_us/src/taskmaster.py &
-roslaunch stdr_launchers among_rviz.launch &
-roslaunch among_us occupancy_grid.launch 
+roslaunch stdr_launchers among_rviz.launch
 ## roslaunch stdr_launchers robot_control.py
